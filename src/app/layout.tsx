863d343ba55ce47409d28e5741d44dabcf94fec9
raman <ramanmaharzan123@gmail.com>
2025-06-15 23:10:09 +0545

    moved header to a component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
 import { Button } from "@/components/ui/button";
 import { Kanban, LucideKanban, RocketIcon } from "lucide-react";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,27 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="fixed top-0 left-0 right-0 z-20 
-          flex justify-between items-center gap-2 
-          px-5 py-2.5 w-full border-b 
-          backdrop-blur bg-background/25 
-          supports-[backdrop-blur]:bg-background/60"
-        >
-          <div>
-            <Button asChild variant={"ghost"} size={"lg"}>
-              <Link href={homePath()}>
-                <Kanban className="size-8" />
-                <h1 className="font-semibold text-lg lg">TicketBounty</h1>
-              </Link>
-            </Button>
-          </div>
-          <div>
-            <Button asChild variant={"default"}>
-              <Link href={ticketsPath()}>Tickets</Link>
-            </Button>
-          </div>
-        </nav>
+        <Header />
+
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
           {children}
         </main>
---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Kanban } from "lucide-react";
+import { homePath, ticketsPath } from "@/paths";
+
+const Header = () => {
+  return (
+    <div>
+      <nav
+        className="fixed top-0 left-0 right-0 z-20 
+          flex justify-between items-center gap-2 
+          px-5 py-2.5 w-full border-b 
+          backdrop-blur bg-background/25 
+          supports-[backdrop-blur]:bg-background/60"
+      >
+        <div>
+          <Button asChild variant={"ghost"} size={"lg"}>
+            <Link href={homePath()}>
+              <Kanban className="size-8" />
+              <h1 className="font-semibold text-lg lg">TicketBounty</h1>
+            </Link>
+          </Button>
+        </div>
+        <div>
+          <Button asChild variant={"default"}>
+            <Link href={ticketsPath()}>Tickets</Link>
+          </Button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
